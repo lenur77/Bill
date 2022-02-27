56e12766d7e5dbc59e2db3e05c4e2ea4eebdb620
lenur77 <100141392+lenur77@users.noreply.github.com>
2022-02-27 15:25:00 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,35 @@
+import Swiper from 'https://unpkg.com/swiper@8/swiper-bundle.esm.browser.min.js'
+
+const swiper = new Swiper('.testimonials__swiper', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 40,
+  //   effect: 'fade',
+  // fadeEffect: {
+  //   crossFade: true
+  // },
+  autoplay: {
+    delay: 5000,
+  },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.testimonials__swiper-button-next',
+      prevEl: '.testimonials__swiper-button-prev',
+    },
+  });
+
+  document.querySelector('.submit-email').addEventListener('mousedown', (e) => {
+    e.preventDefault();
+    document.querySelector('.subscription').classList.add('done');
+    
+  });
+
+  const links = document.querySelectorAll(".menu-header__link");
+  links.forEach((item)=> {
+    item.addEventListener("click", ()=>{
+    const el = document.getElementById(item.getAttribute("data-link"));
+    el.scrollIntoView({behavior:"smooth", block:"end"})
+  })
+})
