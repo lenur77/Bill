02e5d1bae6bf8a3f504309d5ca48b4a2a0264fea
lenur77 <100141392+lenur77@users.noreply.github.com>
2022-02-28 09:05:33 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -25,11 +25,28 @@ const swiper = new Swiper('.testimonials__swiper', {
     document.querySelector('.subscription').classList.add('done');
     
   });
-
+  
   const links = document.querySelectorAll(".menu-header__link");
-  links.forEach((item)=> {
+  links.forEach((item)=>{
     item.addEventListener("click", ()=>{
     const el = document.getElementById(item.getAttribute("data-link"));
-    el.scrollIntoView({behavior:"smooth", block:"end"})
+  
+    el.scrollIntoView({behavior:"smooth", block:"center"});
   })
+});
+
+document.addEventListener("DOMContentLoaded",()=>{
+  let nav = document.getElementById("nav");
+  let current_pos = nav.offsetTop;
+  window.onscroll = ()=>{
+    let window_pos = document.documentElement.scrollTop;
+    if(window_pos>=current_pos){
+      nav.classList.add("sticky");
+    }
+    else{
+      nav.classList.remove("sticky");
+
+    }
+  }
 })
+ 
